1312c0ca74c349f451b60f492814d2019d8d7f4c
abhishekyadav798 <99576491+abhishekyadav798@users.noreply.github.com>
2022-02-19 21:12:13 +0530

    added http request to auto update component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // import {createContext} from 'react';
 import Child1 from './Child1';
+import Component_2_weather from './Component_2_weather';
 import OnClick from './OnClick';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Child1/>
       <OnClick/>
+      <Component_2_weather/>
     </div>
   );
 }
--- src/Child1.js
+++ src/Child1.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Child2 from './Child2';
 import Child3 from './Child3';
 import {useContext} from 'react';
@@ -7,17 +7,31 @@ import {createContext} from 'react';
 
 function Child1(){
     
-    var [initial, finalvalue] = useState("login error"); 
-
+    var [initial, finalvalue] = useState("--"); 
+    const [items, setItems] = useState();
+  
     function button(){
-        finalvalue("login sucessfull")
-        console.log("this is the value")
+ 
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+
+        fetch("http://test", requestOptions)
+        .then(response => response.text())
+        .then(
+            result => finalvalue(result),
+            
+        )
+        .catch(error => console.log('error', error));
+
     }
 
     return(
         <div >
             How the value is passed from Child1 to Child2 and Child3 onclick
         <button onClick={()=>button()}>Click</button>
+        {initial}
         <nameContext.Provider value={initial}>
         <Child2/>
         <Child3/>
